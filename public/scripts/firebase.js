2bab3d9f19ad2c63c01c5e2f8942b39ec162b288
Hector_Martil <hectormartilb@gmail.com>
2025-01-06 23:21:59 -0600

    v1.4

--- public/scripts/firebase.js
+++ public/scripts/firebase.js
@@ -1,17 +1,17 @@
 // Importar las funciones necesarias desde los SDK de Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
 import { getFirestore, collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-analytics.js"; // Asegúrate de incluir el SDK de analytics
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-analytics.js";
 
-// Tu configuración de Firebase
+// Leer configuración desde las variables de entorno
 const firebaseConfig = {
-    apiKey: "AIzaSyA44X-zFwuJdh1neBTI_Fh95ov2a9Q-g4A",
-    authDomain: "rsvp-cumple.firebaseapp.com",
-    projectId: "rsvp-cumple",
-    storageBucket: "rsvp-cumple.appspot.com",
-    messagingSenderId: "381184260037",
-    appId: "1:381184260037:web:e79951978e0da43d54bdac",
-    measurementId: "G-N53Q9E4S6E"
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
+    measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 // Inicializar Firebase
